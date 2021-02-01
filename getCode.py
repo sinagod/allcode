bce23cee36895dc9b583652cb89cba50bbc99e1f
sinagod <76248835+sinagod@users.noreply.github.com>
2021-02-01 12:18:13 +0800

    Update getCode.py

--- getCode.py
+++ getCode.py
@@ -2,8 +2,16 @@ import requests
 import operator
 from functools import reduce
 
-key = '1683239661:AAFcEH4HMuUj1pI4KnJleRZ9tt3gkv8kttU'
-url = f'https://api.telegram.org/bot{key}/getUpdates'
+
+if "TG_KEY" in os.environ:
+    """
+    判断是否运行自GitHub action,"XMLY_SPEED_COOKIE" 该参数与 repo里的Secrets的名称保持一致
+    """
+    print("执行自GitHub action")
+    tg_key = os.environ["TG_KEY"]
+
+# tg_key = '1683239661:AAFcEH4HMuUj1pI4KnJleRZ9tt3gkv8kttU'
+url = f'https://api.telegram.org/bot{tg_key}/getUpdates'
 resp = requests.get(url).json()
 
 for i in resp['result']:
