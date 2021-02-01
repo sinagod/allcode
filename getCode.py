d846e7f2492fd35f54a115ea24e04d4634b68cba
sinagod <76248835+sinagod@users.noreply.github.com>
2021-02-01 12:23:09 +0800

    Update getCode.py

--- getCode.py
+++ getCode.py
@@ -11,17 +11,24 @@ if "TG_KEY" in os.environ:
     tg_key = os.environ["TG_KEY"]
 
 # tg_key = '1683239661:AAFcEH4HMuUj1pI4KnJleRZ9tt3gkv8kttU'
-url = f'https://api.telegram.org/bot{tg_key}/getUpdates'
-resp = requests.get(url).json()
 
-for i in resp['result']:
-    if not (i['message']['text'].rfind('/jdfruit')):
-        try:
-            # print(i['message']['text'].split(' ')[1])
-            with open('jdfruit_code.txt', 'a') as f:
-                f.write(i['message']['text'].split(' ')[1])
-                f.write('\n')
-        except IndexError:
-            pass
-        continue
+def getCode(tg_key):
+    url = f'https://api.telegram.org/bot{tg_key}/getUpdates'
+    resp = requests.get(url).json()
+    for i in resp['result']:
+        if not (i['message']['text'].rfind('/jdfruit')):
+            try:
+                with open('jdfruit_code.txt', 'a') as f:
+                    f.write(i['message']['text'].split(' ')[1])
+                    f.write('\n')
+            except IndexError:
+                pass
+            continue
+
+if __name__ == "__main__":
+    getCode(tg_key)
+
+
+
+
 
